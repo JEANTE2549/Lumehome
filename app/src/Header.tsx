d52c4fb5bec2df250408bb1be7cb13c21297c9f0
Kanatip Jaksarn <Kanatip.jak@gmail.com>
2025-05-04 11:11:08 +0700

    Add footer and header

---
+++ app/src/Header.tsx
@@ -0,0 +1,15 @@
+function Header(){
+
+    return(
+        <header>
+            <h1> My header</h1>
+            <nav>
+                <b>
+                    Hi
+                </b>
+            </nav>
+            <hr></hr>
+        </header>
+    );
+}
+export default Header
